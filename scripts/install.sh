e8a2929a34e6f872cf12533497acb3b7388f5d34
Moheeb Zara <moheebz@amazon.com>
2020-05-12 01:27:24 -0700

    fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -101,6 +101,7 @@ else
   cd /home/pi/Projects/amazon-kinesis-video-streams-webrtc-sdk-c
   mkdir build
   cd build
+  cmake ..
   make
 
   cp /home/pi/Projects/amazon-kinesis-video-streams-webrtc-sdk-c/build/kvsWebrtcClientMasterGstSample /home/pi/Projects/robot/
